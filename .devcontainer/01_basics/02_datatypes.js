643711c32ca153993e8cdda83a9d8475cecbfca1
sidharth001 <51209721+sidharth001@users.noreply.github.com>
2025-04-06 19:45:24 +0000

    datatype changes

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -1,5 +1,5 @@
 "use strict"; //treat all JS code as new version of java code
-alert("Hello") //used to print any message as alert. For arithematic if used in browser, it will give result as there everything is in document form
+//alert("Hello") //used to print any message as alert. For arithematic if used in browser, it will give result as there everything is in document form
 // alert(3+3) // we are using nodejs not browser so it will throw error
 
 console.log(3+3) //will give 6
@@ -7,5 +7,20 @@ console.log(3+3) //will give 6
 
 let name="sidharth"
 let age =26
+let isLoggedin=true
 
-console.log(typeof name);
+//datatypes
+//number =>2 to the ower 53
+//bigint
+//string
+//boolean =>true/false
+//null  => standalone value
+//undefined =>value not defined
+//symbol =>unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefined
+
+
