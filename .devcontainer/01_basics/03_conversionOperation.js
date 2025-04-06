997909e384f9514e0dc2a374ddf8ea6746bc8527
sidharth001 <51209721+sidharth001@users.noreply.github.com>
2025-04-06 20:17:02 +0000

    Conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber= Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+//"33aabc" => NaN
+// true =>1; false>=0
+//null =>0
+// undefined => Nan
+
+let isLoggedIn= 1
+let booleanIsLoggedIn  =Boolean(isLoggedIn)
+console/console.log(booleanIsLoggedIn);
+
+
+//  1 >=true ; 0 >= false
+// "" >= false
+// "hitesh" >=true
+
+
+let someNumber= 33
+
+let strNumber = String(someNumber)
+
+console.log(typeof strNumber);
